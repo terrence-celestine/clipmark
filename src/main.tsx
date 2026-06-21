69102d0b42bfe6e561f0ef4d3648d214b6560015
Terrence <terrence.butler89@gmail.com>
2026-06-21 10:44:56 -0700

    fix: removed seed data on mount

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { seedDatabase } from "./db/seed.ts";
-
-seedDatabase();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
